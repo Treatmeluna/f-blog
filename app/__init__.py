858c891e671ec5191bf7e3f587d5235fc02597d9
treatmeluna <realizemiyu@gmail.com>
2023-12-14 18:30:00 +0900

    modify commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,34 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from flask_moment import Moment
+from config import config
+
+naming_convention = {
+    "ix" : 'ix_%(column_0_labels)s',
+    "uq" : "uq_%(table_name)s_%(column_0_name)s",
+    "ck" : "ck_%(table_name)s_%(column_0_name)s",
+    "fk" : "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk" : "pk_%(table_name)s"
+}
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
+migrate = Migrate()
+moment = Moment()
 
 def create_app():
     app = Flask(__name__)
+
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    db.init_app(app)
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
+
+    moment.init_app(app)
 
     from app.main import main as main_bp
     app.register_blueprint(main_bp)
