d10147d1c893f7277b227d03d505415703cb6cb2
treatmeluna <realizemiyu@gmail.com>
2023-12-15 15:44:23 +0900

    오타 수정

--- myblog.cmd
+++ myblog.cmd
@@ -1,5 +1,5 @@
-@set FLASK_APP = myblog.py
-@set FLASK_ENV = develpoment
-@set FLASK_DEBUG = 1
-@set MYBLOG_ADMIN = realizemiyu@gmail.com
-@set SECRET_KEY = hard_to_guess_string
+@set FLASK_APP=myblog.py
+@set FLASK_ENV=development
+@set FLASK_DEBUG=1
+@set MYBLOG_ADMIN=realizemiyu@gmail.com
+@set SECRET_KEY=hard_to_guess_string
