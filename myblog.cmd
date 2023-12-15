2050fa94c45aa2cf7e3d67d31eb72a725e5b40bb
treatmeluna <realizemiyu@gmail.com>
2023-12-15 10:25:10 +0900

    modify base, add myblog.cmd

---
+++ myblog.cmd
@@ -0,0 +1,5 @@
+@set FLASK_APP = myblog.py
+@set FLASK_ENV = develpoment
+@set FLASK_DEBUG = 1
+@set MYBLOG_ADMIN = realizemiyu@gmail.com
+@set SECRET_KEY = hard_to_guess_string
--- myblog.py
+++ myblog.py
@@ -1,5 +1,5 @@
 import os
 from app import create_app
-from app.module import User
+from app.models import User
 
 app = create_app(os.getenv('MYBLOG_CONFIG') or 'default')
