053d1fb8646af45b46248627dff9d7a887a7ef0d
treatmeluna <realizemiyu@gmail.com>
2023-12-18 11:23:31 +0900

    template 에서 permission 사용

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,4 +2,10 @@ from flask import Blueprint
 
 main = Blueprint('main', __name__)
 
-from .import views
+from .import views, forms
+from ..models import Permission
+
+# template 에서 Permission 을 확인하기 위해
+@main.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
